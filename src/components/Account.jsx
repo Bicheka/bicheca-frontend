c644cc9d7257a28926593098a19472df6ac4e08b
David Martinez Gil <davidmartinezgilp@gmail.com>
2023-04-08 00:13:49 -0400

    added account info to component

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+
 function Account(){
+
+    const userInfo = useSelector(state => state.userInfo.userInfo);
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    
+    useEffect(() => {
+        if(userInfo){
+            setFirstName(userInfo.firstName);
+            setLastName(userInfo.lastName);
+            setEmail(userInfo.email);
+        }
+    }, [userInfo]);
+
     return (
     <div className="home">
         <h1>Account</h1>
-            <button>
-                <div className = "addContact" >
-                    
-                </div>
-            </button>
+        <h3 id='firstName'>{firstName}</h3>
+        <h3 id='lastName'>{lastName}</h3>
+        <h3>Email: {email}</h3>
         
     </div>
     );
---
+++ src/components/Store.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Store() {
+    
+    return (
+        <div className="stores">
+            <h1>Store</h1>
+        </div>
+    );
+}
+
+export default Store;
